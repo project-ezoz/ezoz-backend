17af9699fa08c44595f6272242e799758a44bf48
mistarson <yeowuli2@naver.com>
2022-08-12 00:15:12 +0900

    Update application.yml

--- src/main/java/ezoz/backend_ezoz/global/config/JasyptConfig.java
+++ src/main/java/ezoz/backend_ezoz/global/config/JasyptConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JasyptConfig {
 
-    @Value("${JASYPT_PASSWORD}")
+    @Value("${EZOZ_JASYPT_PASSWORD}")
     String password;
 
     @Bean(name = "jasyptStringEncryptor")
