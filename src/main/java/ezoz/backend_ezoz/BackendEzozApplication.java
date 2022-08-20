b4e6d83628eb048cb68af91e91a7e4e226554b14
mistarson <yeowuli2@naver.com>
2022-08-21 00:14:01 +0900

    initial commit

--- src/main/java/ezoz/backend_ezoz/BackendEzozApplication.java
+++ src/main/java/ezoz/backend_ezoz/BackendEzozApplication.java
@@ -2,8 +2,10 @@ package ezoz.backend_ezoz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BackendEzozApplication {
 
 	public static void main(String[] args) {
--- src/main/java/ezoz/backend_ezoz/api/login/service/social/kakao/KakaoFeignClient.java
+++ src/main/java/ezoz/backend_ezoz/api/login/service/social/kakao/KakaoFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(url = "https://kauth.kakao.com", name = "kakaoTokenFeignClient")
+@FeignClient(url = "https://kauth.kakao.com", name = "kakaoFeignClient")
 public interface KakaoFeignClient {
 
     @GetMapping(value = "/v2/user/me", consumes = "application/json")
