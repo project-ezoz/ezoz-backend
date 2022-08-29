fc5b1f3521453860f1a467a07b940929f0fc4427
mistarson <yeowuli2@naver.com>
2022-08-30 04:03:01 +0900

    Add: Time Zone 설정

--- src/main/java/ezoz/backend_ezoz/BackendEzozApplication.java
+++ src/main/java/ezoz/backend_ezoz/BackendEzozApplication.java
@@ -4,8 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BackendEzozApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("KST"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendEzozApplication.class, args);
