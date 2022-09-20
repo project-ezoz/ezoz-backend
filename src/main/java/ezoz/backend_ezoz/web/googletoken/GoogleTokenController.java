bd1fe8409d89ae57c16a9af48c46cb5dfb14c0bb
mistarson <yeowuli2@naver.com>
2022-09-21 08:00:30 +0900

    소셜 로그인 주소 변경

--- src/main/java/ezoz/backend_ezoz/web/googletoken/GoogleTokenController.java
+++ src/main/java/ezoz/backend_ezoz/web/googletoken/GoogleTokenController.java
@@ -1,12 +1,14 @@
 package ezoz.backend_ezoz.web.googletoken;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class GoogleTokenController {
@@ -34,7 +36,7 @@ public class GoogleTokenController {
     @GetMapping("/auth/google/callback")
     public @ResponseBody
     ResponseEntity<GoogleResponseDto> loginCallback(String code) {
-
+        log.info("구글 리다이렉트 컨트롤러 접속");
         GoogleResponseDto googleResponseDto = googleTokenClient
                 .getGoogleToken(CONTENT_TYPE, clientId, clientSecret, code, GRANT_TYPE, callbackUri);
 
