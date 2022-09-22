dbc6a42a825a401887d2d82a41e933ffcb433bf2
mistarson <yeowuli2@naver.com>
2022-09-22 11:34:41 +0900

    Add: Security 제외 url 추가

--- src/main/java/ezoz/backend_ezoz/global/config/SecurityConfig.java
+++ src/main/java/ezoz/backend_ezoz/global/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     private final TokenManager tokenManager;
     private final ObjectMapper objectMapper;
     private final String[] WHITE_LIST =
-            {"/", "/login", "/api/oauth/login", "/auth/kakao/callback", "/google/login", "/auth/google/callback",
+            {"/", "/kakao/login", "/api/oauth/login", "/auth/kakao/callback", "/google/login", "/auth/google/callback",
                     "/favicon.ico", "/error",
                     "/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**"};
 
