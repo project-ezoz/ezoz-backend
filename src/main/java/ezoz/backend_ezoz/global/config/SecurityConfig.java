5c2a51112b85ad77a27d4a97fff45fbfdd5b3603
mistarson <yeowuli2@naver.com>
2022-10-12 22:16:46 +0900

    Test: 구글 로그인 테스트

--- src/main/java/ezoz/backend_ezoz/global/config/SecurityConfig.java
+++ src/main/java/ezoz/backend_ezoz/global/config/SecurityConfig.java
@@ -24,7 +24,8 @@ public class SecurityConfig {
     private final TokenManager tokenManager;
     private final ObjectMapper objectMapper;
     private final String[] WHITE_LIST =
-            {"/", "/kakao/login", "/api/oauth/login", "/auth/kakao/callback", "/google/login", "/auth/google", "/google/code",
+            {"/", "/kakao/login", "/api/oauth/login", "/auth/kakao/callback",
+                    "/google/login", "/auth/google", "/google/code", "/auth/google/callback",
                     "/favicon.ico", "/error", "/health",
                     "/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**"};
 
