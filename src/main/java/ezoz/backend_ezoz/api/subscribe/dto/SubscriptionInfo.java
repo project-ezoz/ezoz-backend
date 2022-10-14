db0830d6454f3cf09a7cc224784ea565fbe91a1b
mistarson <yeowuli2@naver.com>
2022-10-14 15:10:02 +0900

    Feat: 구독 url - security 제외

--- src/main/java/ezoz/backend_ezoz/api/subscribe/dto/SubscriptionInfo.java
+++ src/main/java/ezoz/backend_ezoz/api/subscribe/dto/SubscriptionInfo.java
@@ -1,10 +1,12 @@
 package ezoz.backend_ezoz.api.subscribe.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@Setter
 public class SubscriptionInfo {
 
     @NotBlank
