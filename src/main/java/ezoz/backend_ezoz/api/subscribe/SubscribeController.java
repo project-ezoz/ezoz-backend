ef4937ceb079a7f816e73a36d6e1eeeae6790088
mistarson <yeowuli2@naver.com>
2022-10-14 15:06:29 +0900

    Feat: 구독 url - security 제외

--- src/main/java/ezoz/backend_ezoz/api/subscribe/SubscribeController.java
+++ src/main/java/ezoz/backend_ezoz/api/subscribe/SubscribeController.java
@@ -2,10 +2,12 @@ package ezoz.backend_ezoz.api.subscribe;
 
 import ezoz.backend_ezoz.api.subscribe.dto.SubscriptionInfo;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api")
 public class SubscribeController {
