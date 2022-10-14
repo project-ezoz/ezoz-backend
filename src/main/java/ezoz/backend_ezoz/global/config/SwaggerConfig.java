0400e4298111177b4b2083c2b0f62bfef6c63f2d
mistarson <yeowuli2@naver.com>
2022-10-14 17:35:09 +0900

    Add: Api 문서 수정 - response Content-Type 추가

--- src/main/java/ezoz/backend_ezoz/global/config/SwaggerConfig.java
+++ src/main/java/ezoz/backend_ezoz/global/config/SwaggerConfig.java
@@ -12,7 +12,9 @@ import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @EnableWebMvc
 @Configuration
@@ -20,6 +22,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
+                .produces(getProduceContentTypes())
                 .useDefaultResponseMessages(false)
                 .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
@@ -31,7 +34,14 @@ public class SwaggerConfig {
                 .tags(new Tag("Account", "소셜 로그인 및 계정 관련 API를 제공"))
                 .tags(new Tag("Token", "토큰 재발급과 로그아웃 관련 API를 제공"))
                 .tags(new Tag("Health", "서버가 살아있는지 죽어있는지 확인하기"))
-                .tags(new Tag("Subscription", "구독자 등록 API를 제공"));
+                .tags(new Tag("Subscription", "구독자 등록 API를 제정"));
+    }
+
+    private Set<String> getProduceContentTypes(){
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+
+        return produces;
     }
 
     private ApiInfo apiInfo() {
